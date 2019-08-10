88a086201ae3c2ded7b20ce6a2f1891287bc0e21
John Montejano <johnmontejano@Johns-MacBook-Pro-2.local>
2019-08-10 10:56:33 -0700

    time and date added

--- js/main.js
+++ js/main.js
@@ -175,6 +175,14 @@ $("#phone").inputmask({"mask": "(999) 999-9999"});
     $("#total-price-text-ipad").text("Our Price:$89")                                                                                             }
 
   }
-
-
-
+ 
+$(function(){     
+  var d = new Date(),        
+      h = d.getHours(),
+      m = d.getMinutes();
+  if(h < 10) h = '0' + h; 
+  if(m < 10) m = '0' + m; 
+  $('input[type="time"][value="now"]').each(function(){ 
+    $(this).attr({'value': h + ':' + m});
+  });
+});
